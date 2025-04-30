192b9dd249a69197ed7e26fc2dc55acfec935e25
hsaenzdev <hectors.digital@gmail.com>
2025-04-29 20:04:09 -0500

    2349827423

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -2,7 +2,6 @@ import { Box } from "@mui/material";
 import { GreetingSection } from "./GreetingSection";
 import { GameObjectiveSection } from "./GameObjectiveSection";
 import { RetroTerminalSection } from "./RetroTerminalSection";
-import { TechIconsSection } from "./TechIconsSection";
 
 export const Home = () => {
   return (
@@ -10,7 +9,7 @@ export const Home = () => {
       display="flex"
       height="100vh"
       flexDirection="column"
-      justifyContent="space-between"
+      // justifyContent="space-between"
     >
       <GreetingSection />
 
@@ -26,64 +25,6 @@ export const Home = () => {
         {/* Terminal Container - Right side */}
         <RetroTerminalSection />
       </Box>
-      {/* <TechIconsSection /> */}
-      <h1>hello there</h1>
     </Box>
-
-
-
   );
-  // return (
-  // <Box
-  //   sx={{
-  //     width: '100%',
-  //     minHeight: '100vh',
-  //     display: 'flex',
-  //     flexDirection: 'column',
-  //     position: 'relative',
-  //     overflow: 'hidden',
-  //   }}
-  // >
-  //   {/* Main Container with flexbox */}
-  //   <Box 
-  //     sx={{ 
-  //       minHeight: '100vh',
-  //       position: 'relative',
-  //       display: 'flex',
-  //       flexDirection: 'column',
-  //       px: { xs: 2, sm: 3, md: 5 },
-  //     }}
-  //   >
-  //     {/* Top greeting text - positioned at top left */}
-  //     <GreetingSection />
-
-  //     {/* Main content area - game objective and terminal */}
-  //     <Box 
-  //       sx={{ 
-  //         flex: 1,
-  //         display: 'flex',
-  //         alignItems: 'center',
-  //         backgroundColor: "teal",
-  //         justifyContent: 'space-between',
-  //       }}
-  //     >
-  //       {/* Game objective section - centered vertically */}
-  //       <GameObjectiveSection />
-
-  //       {/* Terminal Container - Right side */}
-  //       <RetroTerminalSection />
-  //     </Box>
-  //   </Box>
-
-  //   {/* Technology Icons at Bottom Center */}
-  //   <TechIconsSection />
-  // </Box>
-  // );
 };
-
-// Export all components for direct imports
-export * from "./GreetingSection";
-export * from "./GameObjectiveSection";
-export * from "./RetroTerminalSection";
-export * from "./TechIconsSection";
-export * from "./constants"; 
